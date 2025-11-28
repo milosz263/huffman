a2bf0d033aa05259b28f76671aa7d3b429c3db5c
Milosz263 <31561481+milosz263@users.noreply.github.com>
2025-11-28 12:24:16 +0100

    Change approach for data handling

--- src/data.hpp
+++ src/data.hpp
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <string>
-#include <istream>
-
 #include <filesystem>
 namespace fs = std::filesystem;
 
@@ -13,19 +9,3 @@ namespace fs = std::filesystem;
  * 
  */
 typedef uint8_t byte;
-
-/**
- * @brief Load entire contents of file to memory. 
- * 
- * @param filename 
- * @return std::vector<byte> 
- */
-std::vector<byte> loadFile(std::string filename);
-
-/**
- * @brief Converts vector to istream.
- * 
- * @param vec std::vector<byte>
- * @return std::basic_istream<byte> 
- */
-std::basic_istream<byte> vectorToIStream(std::vector<byte> vec);
