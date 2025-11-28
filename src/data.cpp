c2fc3bce3eb58846e04ea8d954a696260451ef78
Milosz263 <31561481+milosz263@users.noreply.github.com>
2025-11-28 11:37:17 +0100

    Finish loadFile(), add vectorToIStream()

--- src/data.cpp
+++ src/data.cpp
@@ -1,11 +1,29 @@
 #include "data.hpp"
+#include <fstream>
 
-std::vector<std::byte> loadFile(std::string filename)
+class bytebuf : public std::basic_streambuf<byte>
+{
+public:
+    bytebuf(std::vector<byte> v)
+    {
+        setbuf(v.data(), v.size());
+    }
+};
+
+std::vector<byte> loadFile(std::string filename)
 {
     fs::path path(filename);
-    std::vector<std::byte> file;
-    file.reserve(fs::file_size(path));
-    std::ifstream input;
+    size_t size = fs::file_size(path);
+    std::vector<byte> file;
+    file.reserve(size);
+    std::basic_ifstream<byte> input;
     input.open(path, std::ios::binary);
+    input.read(file.data(), size);
     return file;
+}
+
+std::basic_istream<byte> vectorToIStream(std::vector<byte> vec)
+{
+    bytebuf buf(vec);
+    return std::basic_istream<byte>(&buf);
 }
--- src/data.hpp
+++ src/data.hpp
@@ -2,18 +2,30 @@
 
 #include <vector>
 #include <string>
-#include <fstream>
+#include <istream>
 
 #include <filesystem>
 namespace fs = std::filesystem;
 
-#include <cstddef>
+#include <cstdint>
+/**
+ * @brief Using uint8 instead of std::byte will be simpler for this use case.
+ * 
+ */
+typedef uint8_t byte;
 
 /**
- * @todo finish implementation and documentation.
- * @brief 
+ * @brief Load entire contents of file to memory. 
  * 
  * @param filename 
  * @return std::vector<byte> 
  */
-std::vector<std::byte> loadFile(std::string filename);
+std::vector<byte> loadFile(std::string filename);
+
+/**
+ * @brief Converts vector to istream.
+ * 
+ * @param vec std::vector<byte>
+ * @return std::basic_istream<byte> 
+ */
+std::basic_istream<byte> vectorToIStream(std::vector<byte> vec);
