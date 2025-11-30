6a355e3abcf5f5991a63b178cba62d65730d8ff9
Milosz263 <31561481+milosz263@users.noreply.github.com>
2025-11-30 18:26:08 +0100

    huffman.hpp

--- headers/huffman.hpp
+++ headers/huffman.hpp
@@ -4,4 +4,12 @@
  * 
  */
 #pragma once
+#include "data.hpp"
+#include "loader.hpp"
+#include "writer.hpp"
 
+namespace huffman
+{
+    void Encode(ILoader &loader, IWriter &writer);
+    void Decode(ILoader &loader, IWriter &writer);
+}
