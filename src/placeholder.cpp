36ba558b19df043a3a9adc4b01c134cea34d723f
Milosz263 <31561481+milosz263@users.noreply.github.com>
2025-12-02 09:47:46 +0100

    Basic Writer and Loader tests

--- src/placeholder.cpp
+++ src/placeholder.cpp
@@ -3,12 +3,31 @@
 #include "huffman.hpp"
 #include <iostream>
 
+static void rewrite(ILoader &loader, IWriter &writer)
+{
+    while (!loader.end())
+    {
+        writer.write(loader.get());
+    }
+    writer.close();
+}
+
+static void rewrite_chunk(ILoader &loader, IWriter &writer)
+{
+    auto size = loader.size();
+    auto ch = loader.getChunk(size);
+    writer.writeChunk(ch);
+    writer.close();
+}
+
 void huffman::Encode(ILoader &loader, IWriter &writer)
 {
-    std::cout << "Calling huffman::Encode()" << std::endl;
+    std::cout << "Calling huffman::Encode(), rewriting by byte" << std::endl;
+    rewrite(loader, writer);
 }
 
 void huffman::Decode(ILoader &loader, IWriter &writer)
 {
-    std::cout << "Calling huffman::Decode()" << std::endl;
+    std::cout << "Calling huffman::Decode() rewriting by chunk" << std::endl;
+    rewrite_chunk(loader, writer);
 }
