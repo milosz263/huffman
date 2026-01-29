1f6a1b90d1d7c181f82fdc9a6d979e30040e1adc
Milosz263 <31561481+milosz263@users.noreply.github.com>
2026-01-29 21:15:36 +0100

    Add UnsupportedFileException

--- headers/huffman.hpp
+++ headers/huffman.hpp
@@ -6,10 +6,15 @@
 #pragma once
 #include "loader.hpp"
 #include "writer.hpp"
+#include <exception>
+
 
 namespace huffman
 {
     void Encode(ILoader &loader, IWriter &writer);
     void Decode(ILoader &loader, IWriter &writer);
+
+    class UnsupportedFileException : public std::exception
+    {};
     
 }
